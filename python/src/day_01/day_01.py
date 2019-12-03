7ce454212dd0ce8a4576b70845a6072770c106cb
jhonhsilva <jhow.pds@hotmail.com>
2019-12-03 11:40:23 -0300

    criação de projeto e adição dos dias 01 e 02 do Advent of Code 2019

---
+++ python/src/day_01/day_01.py
@@ -0,0 +1,18 @@
+from math import floor
+import pandas as pd
+
+
+def get_fuel(mass: float):
+    return floor(mass / 3) - 2
+
+
+def get_fuel_fuel(fuel: float, total_fuel: float = 0):
+    new_fuel = get_fuel(fuel)
+    return get_fuel_fuel(new_fuel, total_fuel + new_fuel) if new_fuel > 0 else total_fuel
+
+
+modules = pd.read_csv('src/day_01/day_01.csv', names=['module'])
+fuel = modules.module.apply(get_fuel).sum()
+total_fuel = modules.module.apply(get_fuel).apply(lambda f: f + get_fuel_fuel(f)).sum()
+print(f'Part one: {fuel}')
+print(f'Part two: {total_fuel}')
---
+++ python/src/day_02/day_02.py
@@ -0,0 +1,36 @@
+def execute_instructions(instructions: list) -> list:
+    for i, instruction in enumerate(instructions):
+        if i % 4 == 0:
+            if instruction == 99:
+                return instructions
+            instructions = operation(instructions, instruction, instructions[i + 1], instructions[i + 2], instructions[i + 3])
+
+
+def operation(instructions: list, operation: int, instruction1: int, instruction2: int, instruction3: int) -> list:
+    instructions[instruction3] = instructions[instruction1] + instructions[instruction2] if operation == 1 else instructions[instruction1] * instructions[instruction2]
+    return instructions
+
+
+def set_noun_and_verb(instructions: list, noun: int, verb: int):
+    instructions[1] = noun
+    instructions[2] = verb
+    return instructions
+
+
+def find_noun_and_verb(instructions: list, expected_value: int) -> int:
+    for noun in range(0, 100):
+        for verb in range(0, 100):
+            test = set_noun_and_verb(instructions[:], noun, verb)
+            if execute_instructions(test)[0] == expected_value:
+                return (100 * noun) + verb
+    return None
+
+
+instructions = []
+with open('src/day_02/day_02.txt', 'r') as file:
+    instructions = list(map(int, file.read().split(',')))
+part_one = set_noun_and_verb(instructions[:], 12, 2)
+part_one = execute_instructions(part_one)[0]
+part_two = find_noun_and_verb(instructions[:], 19690720)
+print(f'Part one: {part_one}')
+print(f'Part two: {part_two}')
