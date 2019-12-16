a25fcdbfa0683bc584f68aa85408d855524bc298
jhonhsilva <jhow.pds@hotmail.com>
2019-12-16 14:51:02 -0300

    adição da parte 2 do dia 5

--- src/day_05/day_05.py
+++ src/day_05/day_05.py
@@ -15,33 +15,52 @@ def execute_instructions(parameters: list) -> list:
     i = 0
     while True:
         parameter = parameters[i]
-        if i % 2 == 0:
-            _, parameter2_mode, parameter1_mode, opcode = get_parameters_modes_and_opcode(parameter)
-            if opcode in [1, 2]:
-                parameter1 = get_value_by_parameter_mode(parameters, parameter1_mode, parameters[i + 1])
-                parameter2 = get_value_by_parameter_mode(parameters, parameter2_mode, parameters[i + 2])
-                parameter3 = parameters[i + 3]
-                parameters = operation(parameters, opcode, parameter1, parameter2, parameter3)
-                i += 4
-            elif opcode in [3, 4]:
-                parameters = operation(parameters, opcode, parameters[i + 1])
-                i += 2
-            elif opcode == 99:
-                return parameters
-            else:
-                i += 1
+        _, parameter2_mode, parameter1_mode, opcode = get_parameters_modes_and_opcode(parameter)
+        if opcode in [1, 2, 7, 8]:
+            parameter1 = get_value_by_parameter_mode(parameters, parameter1_mode, parameters[i + 1])
+            parameter2 = get_value_by_parameter_mode(parameters, parameter2_mode, parameters[i + 2])
+            parameter3 = parameters[i + 3]
+            parameters, _ = operation(parameters, opcode, parameter1, parameter2, parameter3)
+            i += 4
+        elif opcode == 3:
+            parameters, _ = operation(parameters, opcode, parameters[i + 1])
+            i += 2
+        elif opcode == 4:
+            parameter1 = get_value_by_parameter_mode(parameters, parameter1_mode, parameters[i + 1])
+            parameters, _ = operation(parameters, opcode, parameter1)
+            i += 2
+        elif opcode in [5, 6]:
+            parameter1 = get_value_by_parameter_mode(parameters, parameter1_mode, parameters[i + 1])
+            parameter2 = get_value_by_parameter_mode(parameters, parameter2_mode, parameters[i + 2])
+            parameters, position = operation(parameters, opcode, parameter1, parameter2)
+            i = i + 3 if position == 0 else position
+        elif opcode == 99:
+            return parameters
+        else:
+            i += 1
 
 
-def operation(parameters: list, operation: int, parameter1: int, parameter2: int = 0, parameter3: int = 0) -> list:
+def operation(parameters: list, operation: int, parameter1: int, parameter2: int = 0, parameter3: int = 0) -> tuple:
+    position = 0
     if operation == 1:
         parameters[parameter3] = parameter1 + parameter2
     elif operation == 2:
         parameters[parameter3] = parameter1 * parameter2
     elif operation == 3:
-        parameters[parameter1] = int(input('Insert a value: '))
+        parameters[parameter1] = int(input('Input value: '))
     elif operation == 4:
-        print(f'Value in position [{parameter1}]: {parameters[parameter1]}')
-    return parameters
+        print(f'Outupt value: {parameter1}')
+    elif operation == 5:
+        if parameter1 != 0:
+            position = parameter2
+    elif operation == 6:
+        if parameter1 == 0:
+            position = parameter2
+    elif operation == 7:
+        parameters[parameter3] = 1 if parameter1 < parameter2 else 0
+    elif operation == 8:
+        parameters[parameter3] = 1 if parameter1 == parameter2 else 0
+    return parameters, position
 
 
 parameters = []
