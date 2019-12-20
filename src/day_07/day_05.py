aab9ee5ed62e6cd8dd2756e8e04c76e3a7c040d7
jhonhsilva <jhow.pds@hotmail.com>
2019-12-20 18:29:25 -0300

    adição de parte 1 do dia 7 (cópia e modificação do 'intcode' do dia 5 para dia 7)

---
+++ src/day_07/day_05.py
@@ -0,0 +1,97 @@
+def get_parameters_modes_and_opcode(parameter: int):
+    parameter = str(parameter)
+    opcode = parameter[-2:]
+    parameters_mode = parameter[:-2]
+    parameters_mode = ('0' * (3 - len(parameters_mode))) + parameters_mode
+    _, parameter2_mode, parameter1_mode = parameters_mode
+    return [int(value) for value in [0, parameter2_mode, parameter1_mode, opcode]]
+
+
+def get_value_by_parameter_mode(instructions: list, mode: int, parameter_value):
+    return instructions[parameter_value] if mode == 0 else parameter_value
+
+# part one
+# def execute_instructions(parameters: list) -> list:
+#     i = 0
+#     while True:
+#         parameter = parameters[i]
+#         _, parameter2_mode, parameter1_mode, opcode = get_parameters_modes_and_opcode(parameter)
+#         if opcode in [1, 2]:
+#             parameter1 = get_value_by_parameter_mode(parameters, parameter1_mode, parameters[i + 1])
+#             parameter2 = get_value_by_parameter_mode(parameters, parameter2_mode, parameters[i + 2])
+#             parameter3 = parameters[i + 3]
+#             parameters, _ = operation(parameters, opcode, parameter1, parameter2, parameter3)
+#             i += 4
+#         elif opcode == 3:
+#             parameters, _ = operation(parameters, opcode, parameters[i + 1])
+#             i += 2
+#         elif opcode == 4:
+#             parameter1 = get_value_by_parameter_mode(parameters, parameter1_mode, parameters[i + 1])
+#             parameters, _ = operation(parameters, opcode, parameter1)
+#             i += 2
+#         elif opcode == 99:
+#             return parameters
+#         else:
+#             i += 1
+
+# part two
+def execute_instructions(parameters: list, inputs: list = None) -> int:
+    i = 0
+    last_value = None
+    while True:
+        parameter = parameters[i]
+        _, parameter2_mode, parameter1_mode, opcode = get_parameters_modes_and_opcode(parameter)
+        if opcode in [1, 2, 7, 8]:
+            parameter1 = get_value_by_parameter_mode(parameters, parameter1_mode, parameters[i + 1])
+            parameter2 = get_value_by_parameter_mode(parameters, parameter2_mode, parameters[i + 2])
+            parameter3 = parameters[i + 3]
+            parameters, _ = operation(parameters, opcode, parameter1, parameter2, parameter3)
+            i += 4
+        elif opcode == 3:
+            parameters, _ = operation(parameters, opcode, parameters[i + 1], inputs=inputs)
+            i += 2
+        elif opcode == 4:
+            parameter1 = get_value_by_parameter_mode(parameters, parameter1_mode, parameters[i + 1])
+            parameters, _ = operation(parameters, opcode, parameter1, inputs=inputs)
+            last_value = parameter1
+            i += 2
+        elif opcode in [5, 6]:
+            parameter1 = get_value_by_parameter_mode(parameters, parameter1_mode, parameters[i + 1])
+            parameter2 = get_value_by_parameter_mode(parameters, parameter2_mode, parameters[i + 2])
+            parameters, position = operation(parameters, opcode, parameter1, parameter2)
+            i = i + 3 if position == 0 else position
+        elif opcode == 99:
+            return last_value
+        else:
+            i += 1
+
+
+def operation(parameters: list, operation: int, parameter1: int, parameter2: int = 0, parameter3: int = 0, inputs: list = None) -> tuple:
+    position = 0
+    if operation == 1:
+        parameters[parameter3] = parameter1 + parameter2
+    elif operation == 2:
+        parameters[parameter3] = parameter1 * parameter2
+    elif operation == 3:
+        parameters[parameter1] = int(input('Input value: ')) if inputs == None else inputs.pop(0)
+    elif operation == 4:
+        if inputs == None:
+            print(f'Outupt value: {parameter1}')
+    elif operation == 5:
+        if parameter1 != 0:
+            position = parameter2
+    elif operation == 6:
+        if parameter1 == 0:
+            position = parameter2
+    elif operation == 7:
+        parameters[parameter3] = 1 if parameter1 < parameter2 else 0
+    elif operation == 8:
+        parameters[parameter3] = 1 if parameter1 == parameter2 else 0
+    return parameters, position
+
+
+if __name__ == '__main__':
+    with open('src/day_05/day_05.txt', 'r') as file:
+        parameters = list(map(int, file.read().split(',')))
+        
+    execute_instructions(parameters)
--- src/day_07/day_07.py
+++ src/day_07/day_07.py
@@ -1,1 +1,32 @@
-import day_05.day_05
+from day_05 import execute_instructions
+
+
+def get_output_signal(parameters: list, phase_setting: list):
+    output = 0
+    for phase in phase_setting:
+        output = execute_instructions(parameters, inputs=[phase, output])
+    return output
+
+
+def get_highest_signal(parameters: list):
+    highest_signal = 0
+    phase_setting = None
+    r = range(5)
+    for a in r:
+        for b in r:
+            for c in r:
+                for d in r:
+                    for e in r:
+                        phase = [a, b, c, d, e]
+                        if len(set(phase)) < 5:
+                            continue
+                        signal = get_output_signal(parameters, phase)
+                        phase_setting, highest_signal = (phase, signal) if signal > highest_signal else (phase_setting, highest_signal)
+    return highest_signal, phase_setting
+
+
+if __name__ == '__main__':
+    with open('src/day_07/day_07.txt', 'r') as file:
+        parameters = list(map(int, file.read().split(',')))
+    highest_signal = get_highest_signal(parameters)
+    print(f'Part one: {highest_signal}')
